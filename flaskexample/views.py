abc5363a7eaaf7a7bba288d849d986f0b7a31f85
Francine Camacho <camachofrancine@gmail.com>
2019-06-13 18:01:41 -0400

    added heatmap_timeline to local flask app

--- flaskexample/views.py
+++ flaskexample/views.py
@@ -2,7 +2,6 @@ from flask import render_template
 from flaskexample import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-   user = { 'nickname': 'Francine' } # fake user
-   return render_template("index.html", title = 'Home', user = user)
+@app.route('/template/heatmap_santamonica_bird.html')
+def map_data():
+    return render_template('heatmap_santamonica_bird.html')
